44a3628539a033b8e62946ae4993c9cb07ce186e
MyColaAnikeiev <mykolaanikeiev@gmail.com>
2022-04-16 14:53:45 +0300

    Project setup.

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+
+module.exports = {
+    mode: "development",
+    devtool: 'eval-source-map',
+    entry: path.resolve(__dirname,"src/index.ts"),
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /.tsx?$/,
+                loader: 'ts-loader'
+            }
+        ]
+    }
+}
