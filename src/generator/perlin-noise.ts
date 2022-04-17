3f1f432e16eb56948b5c2829e8e4465c1cb4750d
MyColaAnikeiev <mykolaanikeiev@gmail.com>
2022-04-17 23:11:06 +0300

    Interface fix.

--- src/generator/perlin-noise.ts
+++ src/generator/perlin-noise.ts
@@ -1,3 +1,5 @@
+import { PerlinNode } from "./perlin-node";
+
 const permutation = [151,160,137,91,90,15,                 // Hash lookup table as defined by Ken Perlin.  This is a randomly
   131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,    // arranged array of all numbers from 0-255 inclusive.
   190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
@@ -13,7 +15,7 @@ const permutation = [151,160,137,91,90,15,                 // Hash lookup table 
   138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180
 ];
 
-export class PerlinNoise{
+export class PerlinNoise implements PerlinNode{
     /**
      * 
      * @param {number} size - number of x or y units that corresponds to
