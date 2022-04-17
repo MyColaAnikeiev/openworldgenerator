ac5d0ecbbbf2cdfd6bc01c178ab847838ec50078
MyColaAnikeiev <mykolaanikeiev@gmail.com>
2022-04-17 23:26:21 +0300

    Combination node classes that could combine other PerlinNodes.

---
+++ src/generator/perlin-combination-operators.ts
@@ -0,0 +1,39 @@
+import { PerlinNode } from './perlin-node'
+
+/**
+ *  Sums up outputs of `PerlinNode` array.
+ */
+export class PerlinCombine implements PerlinNode{
+    constructor(private sources: PerlinNode[]) {
+    }
+
+    getValue(x: number, y: number): number {
+        return this.sources.reduce( (prev: number, node: PerlinNode) => {
+            return prev + node.getValue(x,y);
+        }, 0)
+    }
+
+    updateSources(sources: PerlinNode[]){
+        this.sources = sources;
+    }
+}
+
+
+type NodeWeightPair = { node: PerlinNode, weight: number};
+/**
+ *  Sums up weighted outputs of provided `PerlinNode` array.
+ */
+export class PerlinCombineWeighted implements PerlinNode{
+    constructor( private sources: NodeWeightPair[] ) 
+    {}
+
+    getValue(x: number, y: number): number {
+        return this.sources.reduce((prev: number, pair: NodeWeightPair) => {
+            return prev + pair.node.getValue(x,y) * pair.weight;
+        }, 0)
+    }
+
+    updateSources(sources: NodeWeightPair[]){
+        this.sources = sources;
+    }
+}
