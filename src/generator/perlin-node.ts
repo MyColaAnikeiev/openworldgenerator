5e82a5d85de63829185ffaec831cbc597f2f5ebf
MyColaAnikeiev <mykolaanikeiev@gmail.com>
2022-04-17 23:08:21 +0300

    Interface.

---
+++ src/generator/perlin-node.ts
@@ -0,0 +1,8 @@
+/**
+ * Provide a common interface for tree like structure of perlin noise sources,
+ * filters and other operators. It should be noted that such operators do not 
+ * check for loops, so such checks should be done at tree building stage.
+ */
+ export abstract class PerlinNode{
+    abstract getValue(x: number, y: number): number;
+}
