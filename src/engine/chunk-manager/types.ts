b5ea94e19a7af8268754cc644ddcc8b75c8a09b1
MyColaAnikeiev <mykolaanikeiev@gmail.com>
2022-05-07 23:07:29 +0300

    Chunk manager related types.

---
+++ src/engine/chunk-manager/types.ts
@@ -0,0 +1,45 @@
+import { Object3D } from "three"
+
+export type Position = {
+    x: number,
+    y: number
+}
+
+export type IndexPosition = {
+    i: number,
+    j: number
+}
+
+export enum ChunkReadyState{
+    stoped, // Not generated. Was canceled
+    pending, // If not stoped, when finished, generator will set state `used`
+    ready, // When it generated but not used.
+    used // When it generated and used
+}
+
+export type ChunkInstance = {
+    // Min and max distance from central chunk that correspond to same simplificationLevel
+    minRound: number, 
+    maxRound: number,
+    // Used for optimization based on distance from central chunk.
+    simplificationLevel: number,
+    // Set stoped when generation need to be canceled. 
+    state: ChunkReadyState,
+    object3D: Object3D | null
+}
+
+export type ChunkArea = { 
+    i: number, 
+    j: number,
+    currentRound: number, // Current distance (manhattan metric) from central chunk
+    instances: ChunkInstance[]
+}
+
+export type ChunkGenState = {
+    readonly chunkArea: ChunkArea,
+    readonly instance: ChunkInstance,
+    // May be used by generation task runner to save intermediate results if task is split midway.
+    generationState: null | {
+        [key:string]: any
+    }
+}
