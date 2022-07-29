1482282ca3be4451d8e9bc539116da336f699476
MyColaAnikeiev <mykolaanikeiev@gmail.com>
2022-07-29 20:34:00 +0300

    feat:Aadd orbital view entity
    You can observe fixed object or scene position by rotating around it,
    or by zooming in and out to it when you attach camera to this entity.

---
+++ src/engine/entities/orbital-view-entity.ts
@@ -0,0 +1,77 @@
+import { EngineUserInterface } from "../engine";
+import { EngineObjects } from "../engine-objects";
+import { TerrainManager } from "../terrain-manager";
+import { Orientation, PlanePosition, Position } from "../types";
+import { Entity } from "./entity";
+
+
+export class OrbitalViewEntity extends Entity{
+    private movementSpeed = 0.01
+    private rotationSpeed = 0.002
+
+    private distance = 0
+    private minViewHeight = 0.6
+    private viewHeight: number
+
+    private terrainManager: TerrainManager;
+    
+    constructor(id: number, hostEngine: EngineUserInterface, engineObjects: EngineObjects, pos: PlanePosition, orientaion: Orientation){
+        super(id, hostEngine, engineObjects, {...pos, height: 0.6}, orientaion)
+
+        this.terrainManager = hostEngine.getTerrainManager()
+        this.viewHeight = this.minViewHeight
+        this.position.height = this.viewHeight + this.terrainManager.getHeight(this.position)
+    }
+
+    /**
+     * @param direction
+     * @param step
+     */
+    public move(direction: string, step: number){
+        const movementSpeed = 0.25
+
+        switch(direction){
+            case 'forward':
+                this.distance = Math.max(0, this.distance - movementSpeed)
+                break
+            case 'beckward':
+                this.distance += movementSpeed
+                break
+            case 'up':
+                this.viewHeight += movementSpeed
+                break
+            case 'down':
+                this.viewHeight = Math.max(0.5, this.viewHeight - movementSpeed)
+                break
+        }
+
+        this.position.height = this.viewHeight + this.terrainManager.getHeight(this.position);
+
+    }
+
+    public rotate(horizontaly: number, verticaly: number){
+        this.orientation.horizontal += horizontaly * this.rotationSpeed;
+        this.orientation.vertical += verticaly * this.rotationSpeed;
+    }
+
+    public getPosition(): Position{
+        const pos = this.position
+        const shiftX = this.distance * Math.sin(this.orientation.horizontal)
+        const shiftY = this.distance * Math.cos(this.orientation.horizontal)
+        const cameraPos = {
+            x: pos.x + shiftX,
+            y: pos.y + shiftY
+        }
+        const cameraHeight = this.terrainManager.getHeight(cameraPos) + this.minViewHeight
+
+        return {
+            ...cameraPos,
+            height: Math.max(pos.height, cameraHeight)
+        }
+    }
+
+    public getOrientaion(): Orientation{
+        return this.orientation;
+    }
+
+}
